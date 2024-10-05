2c021f5e394bbbc0c73aae8b35b5bee3d14f6e0b
Z8264 <zhangbo8264@163.com>
2024-10-05 10:22:53 +0800

    XForm update package

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,7 +2,22 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  platform: 'browser',
-  cjs: { output: 'lib' },
-  esm: { output: 'dist', alias: { 'rc-util/lib': 'rc-util/es' } },
+  // platform: 'browser',
+  // cjs: { output: 'lib' },
+  // esm: { output: 'dist', alias: { 'rc-util/lib': 'rc-util/es' } },
+
+  // ESM 打包配置
+  esm: {
+    input: 'src', // 默认编译目录
+    output: 'es',
+    platform: 'browser', // 默认构建为 Browser 环境的产物
+    transformer: 'babel', // 默认使用 babel 以提供更好的兼容性
+  },
+  // CommonJS 打包配置
+  cjs: {
+    input: 'src', // 默认编译目录
+    output: 'lib',
+    platform: 'browser', // 默认构建为 Node.js 环境的产物
+    transformer: 'babel', // 默认使用 esbuild 以获得更快的构建速度
+  },
 });
